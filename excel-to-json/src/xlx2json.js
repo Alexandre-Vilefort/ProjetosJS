b5e9609e4c8bb417716bf7c1a9420cae6a4ece07
Vilefort <alexandre.vilefot@gmail.com>
2021-03-11 21:02:11 -0300

    nd

--- excel-to-json/src/xlx2json.js
+++ excel-to-json/src/xlx2json.js
@@ -1,31 +1,33 @@
 var selectedFile;
-    var jsonObject;
-    document
-      .getElementById("fileUpload")
-      .addEventListener("change", function(event) {
+var jsonObject;
+var dataQuest;
+document
+    .getElementById("fileUpload")
+    .addEventListener("change", function (event) {
         selectedFile = event.target.files[0];
-      });
-    document
-      .getElementById("uploadExcel")
-      .addEventListener("click", function() {
+    });
+document
+    .getElementById("uploadExcel")
+    .addEventListener("click", function () {
         if (selectedFile) {
-          console.log("hi");
-          var fileReader = new FileReader();
-          fileReader.onload = function(event) {
-            var data = event.target.result;
+            console.log("hi");
+            var fileReader = new FileReader();
+            fileReader.onload = function (event) {
+                var data = event.target.result;
 
-            var workbook = XLSX.read(data, {
-              type: "binary"
-            });
-            workbook.SheetNames.forEach(sheet => {
-              let rowObject = XLSX.utils.sheet_to_row_object_array(
-                workbook.Sheets[sheet]
-              );
-              jsonObject = JSON.stringify(rowObject);
-              //document.getElementById("jsonData").innerHTML = jsonObject;
-              console.log(jsonObject);
-            });
-          };
-          fileReader.readAsBinaryString(selectedFile);
+                var workbook = XLSX.read(data, {
+                    type: "binary"
+                });
+                workbook.SheetNames.forEach(sheet => {
+                    let rowObject = XLSX.utils.sheet_to_row_object_array(
+                        workbook.Sheets[sheet]
+                    );
+                    jsonObject = JSON.stringify(rowObject);
+                    dataQuest = JSON.parse(jsonObject);
+                    //document.getElementById("jsonData").innerHTML = jsonObject;
+                    //console.log(jsonObject);
+                });
+            };
+            fileReader.readAsBinaryString(selectedFile);
         }
-      });
+    });
