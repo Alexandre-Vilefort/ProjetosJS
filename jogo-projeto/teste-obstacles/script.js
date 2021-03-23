c6e07c746610b27576b7f6f22a894fe890bc7b07
Vilefort <alexandre.vilefot@gmail.com>
2021-03-23 10:24:16 -0300

    nd

--- jogo-projeto/teste-obstacles/script.js
+++ jogo-projeto/teste-obstacles/script.js
@@ -7,20 +7,21 @@ var myGameObject4;
 var myGameObject5;
 var ObjectsList = [];
 var TargetsList = [];
-var FPS = 0.5*8.35;
+var FPS = 0.5 * 8.35;
 var RAIO = 10;
 var teste = 0;
 var time1 = performance.now();
 var timeCounter = 0;
 var fpsCounter = 0;
-var setColors = ["red","green","blue"];//["red","green","blue","Orange","black","gray"];
+var setColors = ["red", "green", "blue","Orange"];//["red","green","blue","Orange","black","gray"];
+var colisionCounter = 0;
 
 function startGame() {
     myGameArea.start();
     createTargets();
-    playeyBase = new semiCircle(4*RAIO, "SlateBlue", 395, 700, 0, 2);
+    playeyBase = new semiCircle(4 * RAIO, "SlateBlue", 395, 700, 0, 2);
     //myGameObject1 = new retangle(75, 75, "red", 100, 120);
-    myGameObject1 = new circle(RAIO*2/3, "red", 400, 450, 0, 3);
+    myGameObject1 = new circle(RAIO * 2 / 3, "red", 400, 450, 0, 4);
     //myGameObjectText = new canvasText(30, testeteset, 0, 0);
 }
 var myGameArea = {
@@ -32,7 +33,7 @@ var myGameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         let timeInterval = FPS;
         this.interval = setInterval(updateGameArea, timeInterval);
-        this.fps = 1/timeInterval*1000
+        this.fps = 1 / timeInterval * 1000
         window.addEventListener('keydown', function (event) {
             myGameArea.keys = (myGameArea.keys || []);
             myGameArea.keys[event.keyCode] = true;
@@ -52,7 +53,7 @@ var myGameArea = {
             }
         })
         window.addEventListener('mousedown', function () {
-            let myGameObjectCreator = new circle(RAIO*2/3, setColors[Math.floor(Math.random() * 3)] , myGameArea.x, myGameArea.y, 0, 10);
+            let myGameObjectCreator = new circle(RAIO * 2 / 3, setColors[Math.floor(Math.random() * setColors.length)], myGameArea.x, myGameArea.y, 0, 8);
             myGameObjectCreator.dx = myGameObjectCreator.dx;
             //ObjectsList.push(myGameObjectCreator);    
         })
@@ -65,10 +66,10 @@ var myGameArea = {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
-function createTargets(){
-    for (let i = 1; i <= 20; i++){
-        for (let j = 1; j <= 4; j++){
-            let myGameObjectCreator = new retangle(20, 20, setColors[Math.floor(Math.random() * 3)], 30 * i + 100, 30 * j +50);
+function createTargets() {
+    for (let i = 1; i <= 20; i++) {
+        for (let j = 1; j <= 4; j++) {
+            let myGameObjectCreator = new retangle(20, 20, setColors[Math.floor(Math.random() * setColors.length)], 30 * i + 100, 30 * j + 50);
         }
     }
 }
@@ -97,7 +98,7 @@ function retangle(width, height, color, x, y) {
         if (this.y - height / 2 < 0 || this.y - height / 2 > myGameArea.canvas.height - this.height) {
             this.dy *= -1;
         }
-        if ( this.life == 0 && TargetsList.includes(this) ) {
+        if (this.life == 0 && TargetsList.includes(this)) {
             TargetsList.splice(TargetsList.indexOf(this), 1);
             ObjectsList.splice(ObjectsList.indexOf(this), 1);
             this.x = 300;
@@ -135,7 +136,7 @@ function circle(radius, color, x, y, dx, dy) {
     //this.dx = Math.floor(Math.random() * 11);
     //this.dy = Math.floor(Math.random() * 11);
     this.isInside;
-    this.update = function () { 
+    this.update = function () {
         let ctx = myGameArea.context;
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
@@ -157,9 +158,9 @@ function circle(radius, color, x, y, dx, dy) {
         if (this.y < 0 + this.radius) {
             this.dy *= -1;
             this.isInside = false;
-            if (this.y < 0 + this.radius) { this.y = this.radius } 
+            if (this.y < 0 + this.radius) { this.y = this.radius }
         }
-        if ( this.y > cheidth + 50 && ObjectsList.includes(this) ) {
+        if (this.y > cheidth + 50 && ObjectsList.includes(this)) {
             ObjectsList.splice(ObjectsList.indexOf(this), 1);
             this.x = 300;
             this.y = 300;
@@ -228,7 +229,7 @@ function moveobject_mouse(anyobject) {
     let distx = myGameArea.x - (anyobject.x);
     let disty = myGameArea.y - (anyobject.y);
     let dist = Math.sqrt(distx ** 2 + disty ** 2);
-    let vel = Math.log(dist*3/2);
+    let vel = Math.log(dist * 3 / 2);
 
     if (Math.abs(distx) > 5 && anyobject.isInside) {
         anyobject.dx = vel * distx / dist;
@@ -240,7 +241,7 @@ function moveobject_mouse(anyobject) {
     } else {
         anyobject.dy = 0;
     }
-    
+
     document.getElementById("dist").innerHTML = `distancia mouse e object ${dist.toFixed(2)}`
     //document.getElementById("bluepos").innerHTML = `posicao do obj ${anyobject.x.toFixed(2)}  ${anyobject.y.toFixed(2)}`
     //document.getElementById("bluepos1").innerHTML = `posicao do obj ${anyobject.center[0].toFixed(2)}  ${anyobject.center[1].toFixed(2)}`
@@ -250,19 +251,19 @@ function movePlayerBase_mouse(anyobject) {
     //let disty = myGameArea.y - (anyobject.y);
     let disty = 700 - (anyobject.y); //Travar base numa altura de y
     let dist = Math.sqrt(distx ** 2 + disty ** 2);
-    let vel = 3*Math.log(dist);
+    let vel = 3 * Math.log(dist);
 
     if (Math.abs(distx) > 5 && anyobject.isInside) {
         anyobject.dx = vel * distx / Math.abs(distx);
     } else {
-        anyobject.dx += anyobject.dx/(-3);
+        anyobject.dx += anyobject.dx / (-3);
     }
     if (Math.abs(disty) > 5 && anyobject.isInside) {
         anyobject.dy = vel * disty / Math.abs(disty);
     } else {
-        anyobject.dy += anyobject.dy/(-3);
+        anyobject.dy += anyobject.dy / (-3);
     }
-    
+
     document.getElementById("dist").innerHTML = `PlayerBase x: ${anyobject.x.toFixed(2)} -- y: ${anyobject.y.toFixed(2)}`
     //document.getElementById("bluepos").innerHTML = `posicao do obj ${anyobject.x.toFixed(2)}  ${anyobject.y.toFixed(2)}`
     //document.getElementById("bluepos1").innerHTML = `posicao do obj ${anyobject.center[0].toFixed(2)}  ${anyobject.center[1].toFixed(2)}`
@@ -279,7 +280,7 @@ function collisionDetection(item, list) {
         let par = [item, list[i]]
         let pari = [list[i], item]
         if ((dis_r > 0 && item !== list[i]) && !(colidedPar.includes(par) || colidedPar.includes(pari))) {
-
+            colisionCounter++;
             let a = 2;
             let mangle = Math.atan2(dis_y, dis_x);
             let sin = Math.sin(mangle);
@@ -298,6 +299,7 @@ function collisionDetection(item, list) {
             let v1 = ((item.mass - list[i].mass) * vl1 + 2 * list[i].mass * vl2) / (item.mass + list[i].mass);
             let v2 = ((list[i].mass - item.mass) * vl2 + 2 * item.mass * vl1) / (item.mass + list[i].mass);
 
+            /*
             var tag = document.createElement("p");
             var linebreak = document.createElement('br');
             var text = document.createTextNode(`Colisão: sin - ${(cos.toFixed(2)).toString()}, cos - ${(sin.toFixed(2)).toString()}, Frame: ${fpsCounter}`);
@@ -316,7 +318,7 @@ function collisionDetection(item, list) {
             text = document.createTextNode( `list vel ante: vx ${list[i].dx} -- vy ${list[i].dy}`);
             tag.appendChild(text);
             var element = document.getElementById("debug");
-            element.appendChild(tag);
+            element.appendChild(tag);*/
 
             item.dx = v1 * cos - vp1 * sin;
             item.dy = v1 * sin + vp1 * cos;
@@ -327,6 +329,7 @@ function collisionDetection(item, list) {
             //document.getElementById("debug4").innerHTML = `item vel depois: vx ${item.dx} -- vy ${item.dy}`
             //document.getElementById("debug5").innerHTML = `list vel depois: vx ${list[i].dx} -- vy ${list[i].dy}`
 
+            /*
             var tag = document.createElement("p");
             var linebreak = document.createElement('br');
             var text = document.createTextNode(`item vel depois: vx ${item.dx} -- vy ${item.dy}`);
@@ -336,6 +339,7 @@ function collisionDetection(item, list) {
             tag.appendChild(text);
             var element = document.getElementById("debug");
             element.appendChild(tag);
+            */
 
             if (item.mass > list[i].mass) {
                 list[i].x += (dis_r + a) * cos;
@@ -350,20 +354,17 @@ function collisionDetection(item, list) {
                 item.y -= (dis_r + a / 2) * sin;
             }
 
-            if (item.type != list[i].type){
+            if (item.type != list[i].type) {
                 if (item.color == list[i].color) {
-                    if (item.type == "circle") {
-                        let myGameObjectCreator = new circle(RAIO*2/3, setColors[Math.floor(Math.random() * 3)] , item.x, item.y, Math.floor(Math.random() * 11), Math.floor(Math.random() * 11));
-                        myGameObjectCreator.dx = myGameObjectCreator.dx;
-                    }else{
-                        let myGameObjectCreator = new circle(RAIO*2/3, setColors[Math.floor(Math.random() * 3)] , list[i].x, list[i].y, Math.floor(Math.random() * 11), Math.floor(Math.random() * 11));
-                        myGameObjectCreator.dx = myGameObjectCreator.dx;
-                    }
+
+                    let myGameObjectCreator = new circle(RAIO * 2 / 3, setColors[Math.floor(Math.random() * setColors.length)], myGameArea.x, 350, 0 , 8);
+                    myGameObjectCreator.dx = myGameObjectCreator.dx;
+
                 }
-             
-                if (item.type == "retangle") {item.life += -1};
-                if (list[i].type == "retangle") {list[i].life += -1};
+                if (item.type == "retangle") { item.life += -1 };
+                if (list[i].type == "retangle") { list[i].life += -1 };
             }
+
             colidedPar.push([item, list[i]])
         }
     }
@@ -380,7 +381,8 @@ function textDraw() {
     document.getElementById("fpsCounter").innerHTML = `O FPS é ${myGameArea.fps.toFixed(2)}`
     document.getElementById("showmousepos").innerHTML = `A posição do mouse é ${myGameArea.x}  ${myGameArea.y}`
     document.getElementById("debug1").innerHTML = `O tamanho de ObjectsList é ${ObjectsList.length}`
-    document.getElementById("debug2").innerHTML = `O tamanho de TargetsList é ${TargetsList.length}`
+    //document.getElementById("debug2").innerHTML = `O tamanho de TargetsList é ${TargetsList.length}`
+    document.getElementById("debug3").innerHTML = `O número de colisões é ${colisionCounter}`
 }
 function updateGameArea() {
     myGameArea.clear();
@@ -389,7 +391,7 @@ function updateGameArea() {
     for (let i = 0; i < ObjectsList.length; i++) {
         collisionDetection(ObjectsList[i], ObjectsList);
     }
-    
+
     for (let i = 0; i < ObjectsList.length; i++) {
         ObjectsList[i].update();
     }
