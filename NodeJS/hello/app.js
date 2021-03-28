4d40a0bc49052768af8aa0734c597be6458d5cba
Vilefort <alexandre.vilefot@gmail.com>
2021-03-28 14:19:02 -0300

    Node

---
+++ NodeJS/hello/app.js
@@ -0,0 +1,19 @@
+var msg = 'Hello World';
+console.log('dafa')
+var vector = [];
+vector[0] = 1;
+console.log(vector[0]);
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
