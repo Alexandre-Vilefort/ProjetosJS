707bd5f8d1b229b016d0bf59220fe3967dbb0543
Vilefort <alexandre.vilefot@gmail.com>
2021-04-04 19:26:54 -0300

    nd

--- NodeJS/hello/app.js
+++ NodeJS/hello/app.js
@@ -1,19 +1,22 @@
 var msg = 'Hello World';
-console.log('dafa')
+console.log('teste')
 var vector = [];
 vector[0] = 1;
 console.log(vector[0]);
 
 const readline = require('readline');
 
-const rl = readline.createInterface({
+let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+for (let i = 0; i < 3; i++) {
+  
+  rl.question('What do you think of Node.js? ', (answer) => {
+    // TODO: Log the answer in a database
+    console.log(`Thank you for your valuable feedback: ${answer}`);
 
-  rl.close();
-});
+    rl.close();
+  });
+}
