7367c0f1e2eaf9af34af44ab2878d939bbb8abae
Vilefort <alexandre.vilefot@gmail.com>
2021-04-12 17:56:46 -0300

    nd

---
+++ DesafioVExpenses/Chat/public/scriptChatTest.js
@@ -0,0 +1,55 @@
+const URI = '/api/chat';
+
+const events = new EventSource('http://localhost:8000/api/chat/connect');
+events.onmessage = (event) => {
+    const parsedData = JSON.parse(event.data);
+    console.log(parsedData,'SSE funcionou');
+    $('#testeSSE').html('SSE deu certo');
+    $('#getMessages').click();
+}
+
+
+$('#getMessages').on('click', () => {
+    $.ajax({
+        url: URI,
+        success: function (chatText) {
+            let tbody = $('tbody');
+            tbody.html('');
+            //console.log("chatText:")
+            //console.log(chatText);
+            chatText.forEach(chat => {
+                tbody.append(`
+              <tr>
+                <td class = "name">${chat.name}</td>
+                <td class = "text">${chat.text}</td>
+              </tr>
+          `)
+            })
+        }
+    });
+});
+
+$('#messagesForm').on('submit', (e) => {
+    e.preventDefault();
+    let textsend = $('#newMessage');
+    let namesend = $('#nameUser');
+    $.ajax({
+        url: URI,
+        method: 'POST',
+        data: {
+            name: namesend.val(),
+            text: textsend.val()
+        },
+        success: function (response) {
+            textsend.val('');
+            $('#getMessages').click();
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+});
+
+setInterval(function () {
+    $('#getMessages').click();
+}, 50000);
---
+++ DesafioVExpenses/serverChat.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+///const cors = require('cors');
+
+const app = express();
+
+//setting
+
+app.set('port', process.env.PORT || 8000);
+app.set('host','localhost');
+
+//middlewares
+//app.use(cors());
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// routes
+app.use('/api/chat', require('./routes/apiChatTest'));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//start server
+ app.listen(app.get('port'),function(){
+    console.log(`Server is running on http://${app.get('host')}:${app.get('port')}`);
+    //console.log(`server on port ${app.get('port')}`);
+ });
+
+
