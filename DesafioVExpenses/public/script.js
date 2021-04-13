01c8b2e6566037305a5dccc3095f3f7f74616ee8
Vilefort <alexandre.vilefot@gmail.com>
2021-04-12 23:04:07 -0300

    nd

--- DesafioVExpenses/public/script.js
+++ DesafioVExpenses/public/script.js
@@ -1,7 +1,7 @@
 //const { on } = require("node:events");
-
 const URI = '/api/agenda';
 
+// funcionamento do front
 $(document).ready(function () {
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
@@ -19,26 +19,49 @@ $(document).ready(function () {
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 
+});
+
+//Back-end
+$(document).ready(function () {
     $('#loadContacts').on('click', function () {
         $.ajax({
             url: URI,
             success: function (contacts) {
-                let tbody = $('tbody');
-                tbody.html('');
-                //console.log("chatText:")
-                //console.log(chatText);
+                let container = $('#containerCards');
+                container.html('');
+                //console.log("contacts:")
+                //console.log(contacts);
                 contacts.forEach(contact => {
-                    tbody.append(`
-                  <tr>
-                    <td class = "name">${contact.name}</td>
-                    <td class = "text">${contact.phone}</td>
-                  </tr>`)
+                    container.append(`
+                    <div class="col-lg-6 mb-3">
+                        <div class="card"><div class="card-body"><table>
+                        <tr>
+                            <td><div class="profileImage">B</div></td>
+                            <td><h4 class="card-title">${contact.name}</h4></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><div class="card-text">${contact.phone}</div></td>
+                        </tr>
+                        </table></div></div>
+                    </div>
+                  `)
                 })
             }
         });
-    })
-
+    });
 });
 
 
-
+/* <div class="col-lg-6 mb-3">
+    <div class="card"><div class="card-body"><table>
+        <tr>
+            <td><div class="profileImage">B</div></td>
+            <td><h4 class="card-title">Contato 2</h4></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><div class="card-text">555-555</div></td>
+        </tr>
+    </table></div></div>
+</div> */
