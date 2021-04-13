fcc105268c96b0213079a8f8fd644e9530796225
Vilefort <alexandre.vilefot@gmail.com>
2021-04-13 20:35:13 -0300

    nd

--- DesafioVExpenses/public/script2.js
+++ DesafioVExpenses/public/script2.js
@@ -26,14 +26,17 @@ $(document).ready(function () {
         container.html(`
         <div class="contact-form card col-lg-4">
             <div class="card-body">
-                <form id="newContact-form" >
-                    <input  type="text" class="form-control" id="newNameInp" placeholder="Nome">
+                <form id="newContact-form" action="" >
+                    <label for="newNameInp">Nome</label>
+                    <input  type="text" name="name" class="form-control" id="newNameInp" placeholder="Nome">
                     <br>
-                    <input  type="text" class="form-control" id="newEmailInp" placeholder="Email">
+                    <label for="newEmailInp">Emaio</label>
+                    <input  type="email" name="email" class="form-control" id="newEmailInp" placeholder="Email">
                     <br>
-                    <input  type="text" class="form-control" id="newPhoneInp" placeholder="Phone">
+                    <label for="newPhoneInp">Emaio</label>
+                    <input  type="number" name="phone" class="form-control" id="newPhoneInp" placeholder="Phone">
                     <br>
-                    <button class="form-control btn-dark" id="btnSaveNewContact" onclick="btnSalvar()">Salvar</button>
+                    <button type="submit" class="form-control btn-dark" id="btnSaveNewContact">Salvar</button>
                 </form>
             </div>
         </div>`);
@@ -53,19 +56,16 @@ $(document).ready(function () {
                 contacts.forEach(contact => {
                     container.append(`
                     <div class="col-lg-6 mb-3">
-                    <div class="card">
-                    <div class="card-body form-group">
-                        <form id="newContact-form" >
-                            <input  type="text" class="form-control" id="newNameInp" placeholder="Nome">
-                            <br>
-                            <input  type="text" class="form-control" id="newEmailInp" placeholder="Email">
-                            <br>
-                            <input  type="text" class="form-control" id="newPhoneInp" placeholder="Phone">
-                            <br>
-                            <button type="submit" class="form-control btn-dark" id="btnSaveNewContact">Salvar</button>
-                        </form>
-                    </div>
-                    </div> 
+                        <div class="card"><div class="card-body"><table>
+                        <tr>
+                            <td><div class="profileImage">B</div></td>
+                            <td><h4 class="card-title">${contact.name}</h4></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><div class="card-text">${contact.phone}</div></td>
+                        </tr>
+                        </table></div><i class="fas fa-edit fa-2x text-right"></i></div>
                     </div>
                   `)
                 })
@@ -99,31 +99,31 @@ function btnSalvar(){
 }
 
 
-//  <div class="col-lg-6 mb-3">
-//     <div class="card"><div class="card-body"><table>
-//         <tr>
-//             <td><div class="profileImage">B</div></td>
-//             <td><h4 class="card-title">Contato 2</h4></td>
-//         </tr>
-//         <tr>
-//             <td></td>
-//             <td><div class="card-text">555-555</div></td>
-//         </tr>
-//     </table></div><i class="fas fa-edit text-right"></i></div>
-// </div> 
+{/* <div class="col-lg-6 mb-3">
+    <div class="card"><div class="card-body"><table>
+        <tr>
+            <td><div class="profileImage">B</div></td>
+            <td><h4 class="card-title">Contato 2</h4></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><div class="card-text">555-555</div></td>
+        </tr>
+    </table></div><i class="fas fa-edit text-right"></i></div>
+</div> */}
 
 //btnSaveNewContact
 
-//  <div class="card">
-//             <div class="card-body form-group">
-//                 <form id="newContact-form" >
-//                     <input  type="text" class="form-control" id="newNameInp" placeholder="Nome">
-//                     <br>
-//                     <input  type="text" class="form-control" id="newEmailInp" placeholder="Email">
-//                     <br>
-//                     <input  type="text" class="form-control" id="newPhoneInp" placeholder="Phone">
-//                     <br>
-//                     <button type="submit" class="form-control btn-dark" id="btnSaveNewContact">Salvar</button>
-//                 </form>
-//             </div>
-//         </div> 
+{/* <div class="contact-form card col-lg-4">
+            <div class="card-body">
+                <form id="newContact-form" >
+                    <input  type="text" class="form-control" id="newNameInp" placeholder="Nome">
+                    <br>
+                    <input  type="text" class="form-control" id="newEmailInp" placeholder="Email">
+                    <br>
+                    <input  type="text" class="form-control" id="newPhoneInp" placeholder="Phone">
+                    <br>
+                    <button type="submit" class="form-control btn-dark" id="btnSaveNewContact">Salvar</button>
+                </form>
+            </div>
+        </div> */}
